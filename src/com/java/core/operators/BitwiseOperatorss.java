a4d5757efc1d1bf315897302c3be2a08ed992933
Anwar Mulla <anwar.mulla@nihilent.com>
2021-10-13 12:49:23 +0530

    operators and type

---
+++ src/com/java/core/operators/BitwiseOperatorss.java
@@ -0,0 +1,11 @@
+package com.java.core.operators;
+
+public class BitwiseOperatorss {
+    public static void main(String[] args) {
+        System.out.println(true&false);
+        System.out.println(true|false);
+        System.out.println(false^false);//both same then true else false
+        System.out.println(4&5);//100*101=100=4
+        System.out.println(4|5);//100+101=101=5
+    }
+}
